4b4c24789516a42688bc7584c87652ef7196ba40
Andrew <andrcyes@gmail.com>
2024-08-23 02:01:37 +0700

    Add harpoon, toggleterm

--- init.lua
+++ init.lua
@@ -29,7 +29,7 @@ vim.opt.undofile = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
---vim.opt.termguicolors = true
+vim.opt.termguicolors = true
 
 -- Scroll-off
 vim.opt.scrolloff = 15
@@ -47,6 +47,14 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+-- Default Shell
+vim.opt.shell = 'pwsh'
+vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+vim.opt.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
+vim.opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+vim.opt.shellquote = ""
+vim.opt.shellxquote = ""
 
 -- [[ KEYMAPPING ]]
 -- Netrw
---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,11 @@
+return {
+    'akinsho/toggleterm.nvim',
+    version = "*",
+    opts = {
+        size = 60,
+        open_mapping = [[<C-t>]],
+        hide_numbers = true,
+        name = 'pwsh',
+        direction = 'vertical'
+    }
+}
