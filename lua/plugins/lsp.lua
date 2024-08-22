cf24fb8cac3d1b7ea94eb485d80b65d81ba54a39
Andrew <andrcyes@gmail.com>
2024-08-23 00:23:34 +0700

    Configure ensure_installed LSP servers

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -30,6 +30,9 @@ return {
             })
             require('mason').setup({})
             require('mason-lspconfig').setup({
+                ensure_installed = {
+                    'lua_ls', 'pyright', 'gopls', 'tsserver', 'clangd',
+                },
                 handlers = {
                     function(server_name)
                         require('lspconfig')[server_name].setup({})
