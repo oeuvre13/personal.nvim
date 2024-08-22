423318ac0b479f3177e96963fac80149552b3ed6
Andrew <andrcyes@gmail.com>
2024-08-23 02:35:18 +0700

    Add web-devicons

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,7 +2,10 @@ return {
     {
         'nvim-telescope/telescope.nvim',
         tag = '0.1.8',
-        dependencies = { 'nvim-lua/plenary.nvim' },
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+        },
 
         config = function()
             local builtin = require('telescope.builtin')
