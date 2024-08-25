8f33d1df457055ce9fcb1db88b8498d6841461c5
Andrew <andrcyes@gmail.com>
2024-08-25 19:28:35 +0700

    Add oil

--- init.lua
+++ init.lua
@@ -58,7 +58,7 @@ vim.opt.shellxquote = ""
 
 -- [[ KEYMAPPING ]]
 -- Netrw
-vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+-- vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 
 -- Moving highlighted lines vertically
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
